840834e67fe7e795f38aac667859cbee82f5f1e3
ValerioCarbone <valeriocarbone10@gmail.com>
2023-12-11 17:10:58 +0100

    ADD - Header navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,32 @@
-{{-- <header>
-    <div class="top-bar container">
-        <div class="row d-flex justify-content-end column-gap-2">
-            <div class="col">dc power</div>
-            <div class="col">additional dc sites</div>
+<header>
+    <div class="top-bar bg-primary">
+        <div class="container">
+            <div class="row d-flex flex-row justify-content-end column-gap-2 text-light">
+                <div class="col-2">dc power</div>
+                <div class="col-2">additional dc sites</div>
+            </div>
         </div>
     </div>
-</header> --}}
+    <div class="navbar">
+        <div class="container">
+            <div class="row d-flex flex-row justify-content-between align-items-center flex-nowrap column-gap-4">
+                <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="logo">
+                <ul class=" d-flex justify-content-between">
+                    <li><a href="">characters</a></li>
+                    <li><a href="">comics</a></li>
+                    <li><a href="">movies</a></li>
+                    <li><a href="">tv</a></li>
+                    <li><a href="">games</a></li>
+                    <li><a href="">collectibles</a></li>
+                    <li><a href="">videos</a></li>
+                    <li><a href="">fans</a></li>
+                    <li><a href="">news</a></li>
+                    <li><a href="">shop</a></li>
+                </ul>
+                <form action="POST">
+                    <input type="search" name="" id="">
+                </form>
+            </div>
+        </div>
+    </div>
+</header>
