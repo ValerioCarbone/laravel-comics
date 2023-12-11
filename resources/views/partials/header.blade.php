475ac1368b93fb4cb47d3aa1a9c3683875ee3a1c
ValerioCarbone <valeriocarbone10@gmail.com>
2023-12-11 16:14:50 +0100

    ADD - img + layout + header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <div class="top-bar container">
+        <div class="row">
+            <div class="col">dc power</div>
+            <div class="col">additional dc sites</div>
+        </div>
+    </div>
+</header>
