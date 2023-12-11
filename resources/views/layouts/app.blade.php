4c90a397de2c5077a2d962b04e13a05a41c66b81
ValerioCarbone <valeriocarbone10@gmail.com>
2023-12-11 16:31:40 +0100

    ADD - layout start

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Comics</title>
+    <title>@yield('title')</title>
+    @vite('resources/js/app.js')
 </head>
 
 <body>
+
+    @include('partials.header')
+
+    <main>
+        @yield('content')
+    </main>
+
+    @include('partials.footer')
 
 </body>
 
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,8 @@
-<header>
+{{-- <header>
     <div class="top-bar container">
-        <div class="row">
+        <div class="row d-flex justify-content-end column-gap-2">
             <div class="col">dc power</div>
             <div class="col">additional dc sites</div>
         </div>
     </div>
-</header>
+</header> --}}
